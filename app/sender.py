7644d7499022768bb4dc0ea30e27db223a007965
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2020-04-08 17:35:08 -0300

    Creating environment variables

--- app/sender.py
+++ app/sender.py
@@ -1,6 +1,7 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 
@@ -8,12 +9,15 @@ class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.queue = redis.StrictRedis(host=redis_host, port=6379, db=0)
 
-        self.queue = redis.StrictRedis(host='queue', port=6379, db=0)
-
-        DSN = 'host=db dbname=email_sender user=postgres password=postgres'
-
-        self.conn = psycopg2.connect(DSN)
+        db_host = os.getenv('DB_HOST', 'db')
+        db_name = os.getenv('DB_NAME', 'sender')
+        db_user = os.getenv('DB_USER', 'user')
+        db_password = os.getenv('DB_PASSWORD', 'password')
+        dsn = f'host={db_host} dbname={db_name} user={db_user} password={db_password}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, subject, message):
         SQL = 'INSERT INTO emails(subject,message) VALUES(%s, %s)'
